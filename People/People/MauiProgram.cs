a1cbd6c3b1c0c0cf2b2098150b9b5078b21c5e0c
Диана Ильина <ilinadiana99@gmail.com>
2025-04-10 22:54:17 +0300

    v1

--- People/People/MauiProgram.cs
+++ People/People/MauiProgram.cs
@@ -4,23 +4,21 @@ namespace People;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
+        // Add this code
+        string dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+        builder.Services.AddSingleton<PersonRepository>(s => ActivatorUtilities.CreateInstance<PersonRepository>(s, dbPath));
 
-		// TODO: Add statements for adding PersonRepository as a singleton
-
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,6 +1,9 @@
-﻿using People.Models;
-
+﻿using SQLite;
+using People.Models;
+using System.Threading.Tasks;
 namespace People;
+private SQLiteConnection conn;
+private SQLiteAsyncConnection conn;
 
 public class PersonRepository
 {
@@ -10,9 +13,15 @@ public class PersonRepository
 
     // TODO: Add variable for the SQLite connection
 
-    private void Init()
+
+    private async Task Init()
     {
-        // TODO: Add code to initialize the repository         
+        if (conn != null)
+            return;
+
+        conn = new SQLiteAsyncConnection(_dbPath);
+
+        await conn.CreateTableAsync<Person>();
     }
 
     public PersonRepository(string dbPath)
@@ -20,35 +29,34 @@ public class PersonRepository
         _dbPath = dbPath;                        
     }
 
-    public void AddNewPerson(string name)
-    {            
+    public async Task AddNewPerson(string name)
+    {
         int result = 0;
         try
         {
-            // TODO: Call Init()
+            // Call Init()
+            await Init();
 
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
-            result = 0;
+            result = await conn.InsertAsync(new Person { Name = name });
 
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
         }
         catch (Exception ex)
         {
             StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
         }
-
     }
 
-    public List<Person> GetAllPeople()
+    public async Task<List<Person>> GetAllPeople()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            await Init();
+            return await conn.Table<Person>().ToListAsync();
         }
         catch (Exception ex)
         {
